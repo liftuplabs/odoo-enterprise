1cfac7ddf6e68b0b2e2085485a467db5d3d3748e
tusharm-iprogrammer <tusharm@iprogrammer.co>
2026-06-22 10:29:18 +0530

    [UPDATE]-Remove product create option from invoice line and sale order line

--- custom_invoice_format/__manifest__.py
+++ custom_invoice_format/__manifest__.py
@@ -25,6 +25,7 @@
         'views/res_config_settings_views.xml',
         'views/report_invoice.xml',
         'views/eway_bill_report.xml',
+        'views/account_payment_views.xml',
         'views/report_invoice_with_e_way_bill.xml',
     ],
     'installable': True,
