f4b4dd93ac4c1bcb141e488ccb7ce2aa8ce25589
tusharm-iprogrammer <tusharm@iprogrammer.co>
2026-06-29 18:38:27 +0530

    [UPDATE]-Changes in bulk payment module

--- account_multi_partial_payment/wizard/multi_partial_payment_wizard.py
+++ account_multi_partial_payment/wizard/multi_partial_payment_wizard.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 
 class MultiPartialPaymentWizard(models.TransientModel):
@@ -53,7 +53,12 @@ class MultiPartialPaymentWizard(models.TransientModel):
         """ Iterates through custom wizard lines and processes standard payments. """
         PaymentRegister = self.env['account.payment.register']
 
-        for line in self.line_ids.filtered(lambda l: l.amount_to_pay > 0):
+        for line in self.line_ids:
+            if line.amount_to_pay <= 0:
+                raise ValidationError(
+                    f"The payment amount for invoice {line.move_id.name} must be greater than zero."
+                )
+
             if line.amount_to_pay > line.amount_residual:
                 raise UserError(_("Payment amount cannot exceed the residual amount for invoice %s", line.move_id.name))
 
@@ -95,8 +100,47 @@ class MultiPartialPaymentWizardLine(models.TransientModel):
     _description = 'Multi Partial Payment Wizard Line'
 
     wizard_id = fields.Many2one('multi.partial.payment.wizard')
-    move_id = fields.Many2one('account.move', string="Invoice", readonly=True)
-    partner_id = fields.Many2one('res.partner', string="Customer", readonly=True)
+    move_id = fields.Many2one('account.move', string="Invoice", required=True)
+    partner_id = fields.Many2one('res.partner', string="Customer", required=True)
     currency_id = fields.Many2one('res.currency', related='move_id.currency_id')
     amount_residual = fields.Monetary(string="Amount Due", readonly=True)
     amount_to_pay = fields.Monetary(string="Payment Received")
+    allowed_move_ids = fields.Many2many(
+        'account.move',
+        compute='_compute_allowed_moves'
+    )
+
+    @api.depends('partner_id')
+    def _compute_allowed_moves(self):
+        allowed_move_types = ('out_invoice', 'in_invoice')  # Fallback
+
+        active_ids = self.env.context.get('active_ids')
+        active_model = self.env.context.get('active_model')
+
+        if active_model == 'account.move' and active_ids:
+            selected_moves = self.env['account.move'].browse(active_ids)
+            if selected_moves:
+                if selected_moves[0].move_type == 'in_invoice':
+                    allowed_move_types = ('in_invoice',)  # Lock to Vendor Bills
+                elif selected_moves[0].move_type == 'out_invoice':
+                    allowed_move_types = ('out_invoice',)  # Lock to Customer Invoices
+
+        for line in self:
+            domain = [
+                ('state', '=', 'posted'),
+                ('payment_state', 'in', ('not_paid', 'partial')),
+                ('move_type', 'in', allowed_move_types)
+            ]
+
+            if line.partner_id:
+                domain.append(('partner_id', '=', line.partner_id.id))
+
+            line.allowed_move_ids = self.env['account.move'].search(domain)
+
+    @api.onchange('move_id')
+    def _onchange_move_id(self):
+        for line in self:
+            if line.move_id:
+                line.partner_id = line.move_id.partner_id
+                line.amount_residual = line.move_id.amount_residual
+                line.amount_to_pay = line.move_id.amount_residual  # Default to full payment
